d4a3ce31ea72bd8cb532a556d27c38c114bf0f09
anj.vllmr <144651630+Aenjieee@users.noreply.github.com>
2025-03-11 11:12:01 +0800

    Update add_student_grade.php

--- add_student_grade.php
+++ add_student_grade.php
@@ -65,7 +65,7 @@ if (isset($_POST['add'])) {
                             $mi = $row['mi'];
                             $picture = $row['picture'];
 
-                            // Correted the variable name from $grade to $gradingValue
+                            // Corrected the variable name from $grade to $gradingValue
                             if ($grading == "prelim") {
                                 $gradingValue = $row['prelim'];
                             } elseif ($grading == "midterm") {
